eab64f9f21e5926b59a9b49c93ea5090fda5a081
Bhargav9300 <103308069+Bhargav9300@users.noreply.github.com>
2022-06-26 20:07:10 +0530

    Added problem statement

--- Code/Img_compare_and_segregation.py
+++ Code/Img_compare_and_segregation.py
@@ -1,3 +1,9 @@
+"""Problem Statement : Three folders with images will be provided , all stored with unique names of which some images are 
+                       similar and some are not. For this reason , two new folders are to be created , one containing all 
+                       the unique images in it and the other folder containing the duplicate images.
+"""
+
+
 import cv2 as cv
 import numpy as np
 import os
